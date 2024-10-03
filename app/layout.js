54110629bc753df9b9b8a1324a89a3d8d62058cc
emmeiwhite <emmeiwhite@gmail.com>
2024-10-03 09:18:14 +0530

    Common Logo Added for the site

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import Logo from './components/Logo'
+
 import Navigation from './components/Navigation'
 export const metadata = {
   title: 'The Wild Oasis'
@@ -7,6 +9,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <header>
+          <Logo />
+        </header>
         <Navigation />
         <main>{children}</main>
       </body>
