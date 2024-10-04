929460ab0b11e68fafb3cfc79fe91246332033a1
emmeiwhite <emmeiwhite@gmail.com>
2024-10-04 14:34:28 +0530

    Server Component to Client Component Communication via props passed from the server

--- app/components/Counter.js
+++ app/components/Counter.js
@@ -2,11 +2,17 @@
 import { useState } from 'react'
 
 export default function Counter({ users }) {
+  console.log('use client directive!')
+  console.log(users)
   const [counter, setCounter] = useState(0)
   return (
     <div>
       <button onClick={() => setCounter(c => c + 1)}>{counter}</button>
-      <p>There are a total of {users.length} users</p>
+      <h3>There are a total of {users.length} users</h3>
+
+      {users.map(user => {
+        return <div key={user.id}>{user.name}</div>
+      })}
     </div>
   )
 }
