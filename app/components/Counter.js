62bd01ccf955f777b2ce20ab5ba4b39951c52604
emmeiwhite <emmeiwhite@gmail.com>
2024-10-04 13:37:44 +0530

    Counter is now a React Client Component!

--- app/components/Counter.js
+++ app/components/Counter.js
@@ -1,6 +1,11 @@
+'use client'
 import { useState } from 'react'
 
 export default function Counter() {
   const [counter, setCounter] = useState(0)
-  return <div>Counter</div>
+  return (
+    <div>
+      <button onClick={() => setCounter(c => c + 1)}>{counter}</button>
+    </div>
+  )
 }
