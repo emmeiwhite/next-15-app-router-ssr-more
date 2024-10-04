23655baefb988c407116b02b6930a96cbc8725bb
emmeiwhite <emmeiwhite@gmail.com>
2024-10-04 07:12:53 +0530

    Data Fetching on the server directly with RSC

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,10 +2,21 @@ export const metadata = {
   title: 'Cabins'
 }
 
-export default function Cabins() {
+export default async function Cabins() {
+  // RSC : By Default
+  // Fetching data in RSC: This is done on the server itself & not on the client side!
+
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await res.json()
+  console.log(data)
   return (
     <div>
       <h3>Cabins - The Wild Oasis</h3>
+      <ul>
+        {data.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
