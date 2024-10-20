0c67e9a8a4051f3d83c9de5a7b43152ed13546e3
emmeiwhite <emmeiwhite@gmail.com>
2024-10-20 10:00:04 +0530

    Never says Never!!

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,11 @@
-import Logo from './components/Logo'
-
-import Navigation from './components/Navigation'
 export const metadata = {
   title: 'The Wild Oasis'
 }
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-      </body>
+    <html>
+      <body>{children}</body>
     </html>
   )
 }
