994ca2a7f856ae6905d6d18a7810294f0a24f66c
emmeiwhite <emmeiwhite@gmail.com>
2024-10-21 20:40:03 +0530

    fetching data directly in the RSC on the Server - this is crazy stuff indeed!

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,7 +1,19 @@
-export default function Cabins() {
+const url = 'https://jsonplaceholder.typicode.com/users'
+export default async function Cabins() {
+  // Page is a React Server Component, so we can fetch data directly from the server and then assemble the page on the server itself. Let's do a demo!
+  const response = await fetch(url)
+  const data = await response.json()
+  console.log(data)
+
   return (
     <div>
       <h1>Cabins Page</h1>
+
+      <ul>
+        {data.map(user => {
+          return <li key={user.id}>{user.name}</li>
+        })}
+      </ul>
     </div>
   )
 }
