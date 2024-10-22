cf6800954473099fb7f8c5835df5bc1b12626265
emmeiwhite <emmeiwhite@gmail.com>
2024-10-22 11:52:36 +0530

    Server Component to Client Component communication via props --- but everything is still rendered on the server and sent back as response full HTML page with data

--- app/components/Counter.js
+++ app/components/Counter.js
@@ -12,11 +12,16 @@ export default function Counter({ users }) {
 
       <button onClick={() => setCount(c => c + 1)}>update count</button>
 
-      <div>
+      {/* <div>
         <h3> Always Remember a General Rule:</h3>
         <p>In Pure React: UI is the reflection of the Application State! </p>
         <p>And state updates trigger a re-render of the Component along with its children</p>
-      </div>
+      </div> */}
+      <ul>
+        {users.map(user => {
+          return <li key={user.id}>{user.name}</li>
+        })}
+      </ul>
     </div>
   )
 }
