544a921cb4caca1c5f93602d4094b28a08a1ba16
emmeiwhite <emmeiwhite@gmail.com>
2024-10-22 11:45:32 +0530

    'use client' directive makes a Component a Client Component!

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,5 @@
+import Counter from '../components/Counter'
+
 const url = 'https://jsonplaceholder.typicode.com/users'
 export default async function Cabins() {
   // Page is a React Server Component, so we can fetch data directly from the server and then assemble the page on the server itself. Let's do a demo!
@@ -14,6 +16,8 @@ export default async function Cabins() {
           return <li key={user.id}>{user.name}</li>
         })}
       </ul>
+
+      <Counter />
     </div>
   )
 }
--- app/components/Counter.js
+++ app/components/Counter.js
@@ -5,7 +5,14 @@ export default function Counter() {
   const [count, setCount] = useState(0)
   return (
     <div>
-      <p>This is a React Client Component!</p>
+      <h2>This is a React Client Component! </h2>
+      <div>Current Count: {count}</div>
+
+      <div>
+        <h3> Always Remember a General Rule:</h3>
+        <p>In Pure React: UI is the reflection of the Application State! </p>
+        <p>And state updates trigger a re-render of the Component along with its children</p>
+      </div>
     </div>
   )
 }
